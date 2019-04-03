2c945c691bc936a3cdc11cf719b4045ccee5c9cf
unknown <v.dziun@gmail.com>
2019-04-02 20:20:07 -0700

    first commit

---
+++ assets/Scripts/LookAt.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(CharacterController))]
+
+public class LookAt : MonoBehaviour {
+
+	public Transform target;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		// if no target specified, assume the player
+		if (target == null) {
+			
+			if (GameObject.FindWithTag ("Player")!=null)
+			{
+				target = GameObject.FindWithTag ("Player").GetComponent<Transform>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+		
+		// face the target
+		transform.LookAt(target);
+		
+		//get the distance between the chaser and the target
+	}
+	
+	// Set the target of the chaser
+	public void SetTarget(Transform newTarget)
+	{
+		target = newTarget;
+	}
+	
+}
---
+++ assets/Scripts/ProjectileDestrctor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileDestrctor : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	public GameObject explosionPrefabBoss;
+
+	void OnCollisionEnter (Collision newCollision)
+	{
+
+
+		if (newCollision.gameObject.tag == "Projectile"){
+		if (explosionPrefab) {
+			// Instantiate an explosion effect at the gameObjects position and rotation
+				Instantiate (explosionPrefab, newCollision.gameObject.transform.position, newCollision.gameObject.transform.rotation);
+		}
+
+			Destroy (newCollision.gameObject);
+
+	}
+
+		if (newCollision.gameObject.tag == "Projectile_Boss")
+		{
+		
+			if (explosionPrefabBoss) 
+			{
+				// Instantiate an explosion effect at the gameObjects position and rotation
+				Instantiate (explosionPrefabBoss, newCollision.gameObject.transform.position, newCollision.gameObject.transform.rotation);
+			}
+			
+			Destroy (newCollision.gameObject);
+
+		}
+	}
+
+
+	}
